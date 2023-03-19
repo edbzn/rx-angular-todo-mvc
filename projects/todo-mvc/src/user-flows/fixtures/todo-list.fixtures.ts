09551f375c7e02db2681f8d1be73b83e072775ba
Edouard Bozon <bozonedouard@gmail.com>
2023-03-19 19:54:45 +0100

    test: add user-flow test

---
+++ projects/todo-mvc/src/user-flows/fixtures/todo-list.fixtures.ts
@@ -0,0 +1,2 @@
+export const todoSelector = (idx: number) => `*[data-uf="todo-${idx}"]`;
+export const newTodoSelector = `*[data-uf="new-todo"]`;
---
+++ projects/todo-mvc/src/user-flows/user-flow.ts
@@ -0,0 +1,56 @@
+import {
+  UserFlowContext,
+  UserFlowInteractionsFn,
+  UserFlowOptions,
+  UserFlowProvider
+} from '@push-based/user-flow';
+import { readBudgets } from '@push-based/user-flow/src/lib/commands/assert/utils/budgets';
+import { TodoListUfo } from './ufo/todo-list.ufo';
+
+const flowOptions: UserFlowOptions = {
+  name: 'Basic user flow to ensure MVC functionality',
+};
+
+const listBudgets = readBudgets(
+  './projects/todo-mvc/src/user-flows/configs/budgets.json'
+);
+
+const interactions: UserFlowInteractionsFn = async (
+  ctx: UserFlowContext
+): Promise<any> => {
+  const { flow } = ctx;
+  const url = `/`;
+  const todoList = new TodoListUfo(ctx);
+
+  await flow.navigate(url, {
+    stepName: '🧭 Initial navigation',
+    config: {
+      extends: 'lighthouse:default',
+      settings: {
+        budgets: listBudgets,
+      },
+    },
+  });
+  await todoList.awaitLCPContent();
+  await todoList.awaitAllContent();
+  await flow.snapshot({
+    stepName: '✔ Initial navigation done',
+  });
+  await flow.startTimeSpan({
+    stepName: '🧭 Add new todo',
+  });
+  await todoList.createNewTodo(2, 'Do my homework');
+  await flow.endTimeSpan();
+  await flow.snapshot({
+    stepName: '✔ Add new todo done',
+  });
+
+  return Promise.resolve();
+};
+
+const userFlowProvider: UserFlowProvider = {
+  flowOptions,
+  interactions,
+};
+
+module.exports = userFlowProvider;
