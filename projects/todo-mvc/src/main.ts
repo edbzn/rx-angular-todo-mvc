10d9e31ff693ec807b9084800420e2d944e7deb2
Edouard Bozon <bozonedouard@gmail.com>
2023-05-06 10:59:21 +0200

    refactor: cleanup config

--- projects/todo-mvc/src/main.ts
+++ projects/todo-mvc/src/main.ts
@@ -1,17 +1,11 @@
 import { HttpClientModule } from '@angular/common/http';
 import {
   NgZone,
-  enableProdMode,
   importProvidersFrom,
-  ɵNoopNgZone,
+  ɵNoopNgZone
 } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
-import { environment } from './environments/environment';
-
-if (environment.production) {
-  enableProdMode();
-}
 
 bootstrapApplication(AppComponent, {
   providers: [
