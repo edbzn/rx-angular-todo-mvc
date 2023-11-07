ee9b46d40837bb91aa99dc69e0fa49d4c5ce993d
Edouard Bozon <bozonedouard@gmail.com>
2023-11-07 09:41:44 +0100

    refactor: add footer component

--- projects/todo-mvc/src/app/app.component.ts
+++ projects/todo-mvc/src/app/app.component.ts
@@ -2,19 +2,18 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { TodoListComponent } from './todo-list.component';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-footer',
   changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: true,
-  imports: [TodoListComponent],
   template: `
-    <app-todo-list />
     <footer>
       <strong>Fully zoneless Angular app</strong> using
       <code>
         <a
           href="https://github.com/rx-angular/rx-angular/blob/master/libs/state/README.md"
         >
-          &#64;rx-angular/state</a>
+          &#64;rx-angular/state</a
+        >
       </code>
       to manage state and
       <code>
@@ -25,6 +24,18 @@ import { TodoListComponent } from './todo-list.component';
       >
       to detect changes.
     </footer>
+  `,
+}) class FooterComponent {}
+
+
+@Component({
+  selector: 'app-root',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  standalone: true,
+  imports: [TodoListComponent, FooterComponent],
+  template: `
+    <app-todo-list />
+    <app-footer />
   `,
 })
 export class AppComponent {}
